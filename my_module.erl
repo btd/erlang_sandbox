555a0a7a1946858356652d14145f0cf26d32955b
btd <bardadymchik@gmail.com>
2012-01-22 17:33:21 +0400

    Added tailrev and first version of rpc server
    Signed-off-by: btd <bardadymchik@gmail.com>

--- my_module.erl
+++ my_module.erl
@@ -1,10 +1,23 @@
 %% simple erlang module
 
 -module(my_module).
--export([pie/0, print/1]).
+-export([pie/0, print/1, do_sum/1, rev/1, tailrev/1]).
 
 pie() ->
 	3.14.
 
 print(Term) ->
 	io:format("The value of term is: ~w.~n", [Term]).
+
+do_sum(N) -> do_sum(N, 0).
+
+do_sum(N, Total) when N =/= 0 -> do_sum(N-1, Total + N);
+do_sum(0, Total) -> Total.
+
+rev([]) -> [];
+rev([A|Rest]) -> rev(Rest) ++ [A].
+
+tailrev(X) -> tailrev(X, []).
+
+tailrev([ X | Rest ], Acc) -> tailrev(Rest, [X | Acc]);
+tailrev([], Acc) -> Acc.
